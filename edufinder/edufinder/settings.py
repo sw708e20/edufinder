aad19a4e01158890080de5fa4f4ef6e52c1e268d
Badgie <jonaskhansen@gmail.com>
2020-10-07 11:02:38 +0200

    add some server settings

--- edufinder/edufinder/settings.py
+++ edufinder/edufinder/settings.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+
+import os
+
 from . import secrets
 from pathlib import Path
 
@@ -118,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(f'{BASE_DIR}/static/')
