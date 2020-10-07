3d25a702764306377798135acbb50cf031884746
Badgie <jonaskhansen@gmail.com>
2020-10-07 10:26:05 +0200

    oops

--- edufinder/edufinder/settings.py
+++ edufinder/edufinder/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+from . import secrets
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(16mxrw=w3!7e2zjwy*o31(vd94p$6n)_8r502(s6dxk%+$l!p'
+SECRET_KEY = secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
