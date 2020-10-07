a5739ace08ce4863db402aca5eced348e2f92e5e
root <root@p7-sw708e20.srv.aau.dk>
2020-10-07 12:48:11 +0200

    fikks

--- edufinder/edufinder/settings.py
+++ edufinder/edufinder/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = secrets.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["edufinder.dk"]
 
 
 # Application definition
