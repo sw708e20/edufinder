dbf8808feae40dd5d9814a14a25e5390bef96ae5
Badgie <jonaskhansen@gmail.com>
2020-10-14 11:44:04 +0200

    initial rest api setup

--- edufinder/edufinder/settings.py
+++ edufinder/edufinder/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(f'{BASE_DIR}/static/')
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
