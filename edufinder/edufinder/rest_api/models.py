6e250a979df856fc24dff650100d5bb61ece3b6a
Nicklas Hjortshøj Hugøy <Nicklas.hug@gmail.com>
2020-10-22 12:10:59 +0200

    Add answer model (#8)

--- edufinder/edufinder/rest_api/models.py
+++ edufinder/edufinder/rest_api/models.py
@@ -15,3 +15,22 @@ class EducationType(models.Model):
     education = models.ForeignKey(to=Education, on_delete=models.CASCADE)
     url = models.CharField(max_length=2000)
     name = models.CharField(max_length=120)
+
+
+class AnswerChoice(models.TextChoices):
+    YES = 'Yes'
+    NO = 'No'
+    PROBABLY = 'Probably'
+    PROBABLY_NOT = "Probably not"
+    DONT_KNOW = "Don't know"
+
+class UserAnswer(models.Model):
+    pass
+
+class Answer(models.Model):
+    question = models.ForeignKey(to=Question, on_delete=models.CASCADE)
+    answer = models.CharField(choices=AnswerChoice.choices, max_length=20)
+    userAnswer = models.ForeignKey(to=UserAnswer, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('question', 'userAnswer')
