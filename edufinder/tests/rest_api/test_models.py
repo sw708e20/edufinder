118b6463f35b455fe5916a63fd0fbeca2d457b37
Badgie <jonaskhansen@gmail.com>
2020-10-28 13:41:17 +0100

    fix not null error on user answer

--- edufinder/tests/rest_api/test_models.py
+++ edufinder/tests/rest_api/test_models.py
@@ -8,14 +8,14 @@ class AnswerTests(TestCase):
 
     def test_answer_is_related_to_user_answer(self):
         question = Question.objects.create(question="What is the answer to life, the universe and everything?")
-        userAnswer = UserAnswer.objects.create()
+        userAnswer = UserAnswer.objects.create(ip_addr='8.8.8.8')
         answer = Answer.objects.create(question=question, answer=AnswerChoice.YES, userAnswer=userAnswer)
 
         self.assertEqual(answer, UserAnswer.objects.first().answer_set.first())
 
     def test_user_cannot_answer_same_question(self):
         question = Question.objects.create(question="What is the answer to life, the universe and everything?")
-        userAnswer = UserAnswer.objects.create()
+        userAnswer = UserAnswer.objects.create(ip_addr='8.8.8.8')
         answer = Answer.objects.create(question=question, answer=AnswerChoice.YES, userAnswer=userAnswer)
 
         with self.assertRaises(ValidationError):
