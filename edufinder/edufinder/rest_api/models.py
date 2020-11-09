494c8a4649229ab93ccf067328562c194ff055df
Nicklashugoy <nicklas.hug@gmail.com>
2020-11-09 09:48:06 +0100

    Use IntegerChoices in model and add migration

--- edufinder/edufinder/rest_api/models.py
+++ edufinder/edufinder/rest_api/models.py
@@ -18,12 +18,12 @@ class EducationType(models.Model):
     name = models.CharField(max_length=120)
 
 
-class AnswerChoice(models.TextChoices):
-    YES = 'Yes'
-    NO = 'No'
-    PROBABLY = 'Probably'
-    PROBABLY_NOT = "Probably not"
-    DONT_KNOW = "Don't know"
+class AnswerChoice(models.IntegerChoices):
+    YES = 2
+    NO = -2
+    PROBABLY = 1
+    PROBABLY_NOT = -1
+    DONT_KNOW = 0
 
 class UserAnswer(models.Model):
     datetime = models.DateTimeField(auto_now_add=True)
@@ -32,7 +32,7 @@ class UserAnswer(models.Model):
 
 class Answer(models.Model):
     question = models.ForeignKey(to=Question, on_delete=models.CASCADE)
-    answer = models.CharField(choices=AnswerChoice.choices, max_length=20)
+    answer = models.IntegerField(choices=AnswerChoice.choices)
     userAnswer = models.ForeignKey(to=UserAnswer, on_delete=models.CASCADE)
 
     class Meta:
@@ -41,7 +41,7 @@ class Answer(models.Model):
 class AnswerConsensus(models.Model):
     education = models.ForeignKey(to=Education, on_delete=models.CASCADE)
     question = models.ForeignKey(to=Question, on_delete=models.CASCADE)
-    answer = models.CharField(choices=AnswerChoice.choices, max_length=20)
+    answer = models.IntegerField(choices=AnswerChoice.choices)
 
     class Meta:
         unique_together = [['education', 'question']]
