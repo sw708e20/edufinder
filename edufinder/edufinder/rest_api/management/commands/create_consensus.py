67262b229388dc5c4c808b5da4207cd371b7d97c
Badgie <jonaskhansen@gmail.com>
2020-11-30 09:12:32 +0100

    shorten and split consensus commands

--- edufinder/edufinder/rest_api/management/commands/create_consensus.py
+++ edufinder/edufinder/rest_api/management/commands/create_consensus.py
@@ -1,16 +1,12 @@
 from django.core.management.base import BaseCommand
 from edufinder.rest_api.models import AnswerConsensus, Education, Question, AnswerChoice
 
+
 class Command(BaseCommand):
     help = 'Creates the initial answer consensus'
 
     def handle(self, *args, **options):
-        educations = Education.objects.all()
-        questions = Question.objects.all()
-        acs = []
-        for education in educations:
-            for question in questions:
-                ac = AnswerConsensus(question=question, education=education, answer=AnswerChoice.DONT_KNOW)
-                acs.append(ac)
-
-        AnswerConsensus.objects.bulk_create(acs)
+        AnswerConsensus.objects.bulk_create([
+            AnswerConsensus(question=question, education=education, answer=AnswerChoice.DONT_KNOW)
+            for education in Education.objects.all() for question in Question.objects.all()
+        ])
--- edufinder/edufinder/rest_api/management/commands/update_consensus.py
+++ edufinder/edufinder/rest_api/management/commands/update_consensus.py
@@ -2,40 +2,38 @@ from django.core.management.base import BaseCommand
 from edufinder.rest_api.models import AnswerConsensus, UserAnswer, Answer, AnswerChoice
 from edufinder.rest_api.serializers import AnswerChoiceSerializer
 
+
 class Command(BaseCommand):
     help = 'Updated the answer consensus'
 
     serializer = AnswerChoiceSerializer()
 
     def handle(self, *args, **options):
-        consensus = {}
-
-        #Iterate over all answers and create a dict for each education and each question under that.
-        answers = Answer.objects.all()
-        for answer in answers:
-            user_answer = answer.userAnswer
-            if not consensus.get(user_answer.education.pk):
-                consensus[user_answer.education.pk] = {}
-
-            current_education_consensus = consensus[user_answer.education.pk]
-
-            if not current_education_consensus.get(answer.question.pk):
-                current_education_consensus[answer.question.pk] = {2:0, 1:0, 0:0, -1:0, -2:0}
-
-            current_question_consensus = current_education_consensus[answer.question.pk]
-            int_answer = self.serializer.to_representation(answer.answer)
-
-            current_question_consensus[int_answer] += 1
-
+        consensus = self._create_consensus_dict()
         updated_answer_consensus = []
-        #Iterate over all elements and set what the answer should be.
+
+        # Iterate over all elements and set what the answer should be.
         for education_key, education_info in consensus.items():
             for question_key, answers in education_info.items():
-                int_value = max(answers, key=answers.get)
-                consensus_answer = self.serializer.to_internal_value(int_value)
 
-                ac = AnswerConsensus.objects.filter(education__pk=education_key,question__pk=question_key).get()
-                ac.answer = consensus_answer
+                ac = AnswerConsensus.objects.filter(education__pk=education_key,
+                                                    question__pk=question_key).get()
+                ac.answer = self.serializer.to_internal_value(max(answers, key=answers.get))
                 updated_answer_consensus.append(ac)
 
         AnswerConsensus.objects.bulk_update(updated_answer_consensus, ['answer'])
+
+    def _create_consensus_dict(self) -> dict:
+        consensus = {}
+        # Iterate over all answers and create a dict for each education and each question under that
+        for answer in Answer.objects.all():
+            edu_pk, q_pk = answer.userAnswer.education.pk, answer.question.pk
+
+            if not consensus.get(edu_pk):
+                consensus[edu_pk] = {}
+
+            if not consensus[edu_pk].get(q_pk):
+                consensus[edu_pk][q_pk] = {2: 0, 1: 0, 0: 0, -1: 0, -2: 0}
+
+            consensus[edu_pk][q_pk][self.serializer.to_representation(answer.answer)] += 1
+        return consensus
