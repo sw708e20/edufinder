d209a58a9d1b7bf290442aa71793ca3f94c998d8
Nicklashugoy <nicklas.hug@gmail.com>
2020-12-10 10:25:45 +0100

    Save timestamp with decision tree and print previous when calling command

--- edufinder/edufinder/rest_api/management/commands/create_decision_tree.py
+++ edufinder/edufinder/rest_api/management/commands/create_decision_tree.py
@@ -1,9 +1,12 @@
 from django.core.management.base import BaseCommand
 from edufinder.rest_api.management.question_prioritization import create_question_tree
+from django.core.cache import cache
 
 class Command(BaseCommand):
     help = 'Creates a decision tree to be used for question prioritization'
 
     def handle(self, *args, **options):
+        timestamp = cache.get('question_tree_timestamp')
+        print(f'Previous cache timestamp: {timestamp}')
         create_question_tree()
         
